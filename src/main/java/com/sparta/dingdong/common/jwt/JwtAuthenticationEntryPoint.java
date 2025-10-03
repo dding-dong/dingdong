bf8114f1107382322da57185828b38828188b409
believeme <cain1227@naver.com>
2025-10-03 09:51:04 +0900

    feat: #17 시큐리티, JWT 적용

---
+++ src/main/java/com/sparta/dingdong/common/jwt/JwtAuthenticationEntryPoint.java
@@ -0,0 +1,35 @@
+package com.sparta.dingdong.common.jwt;
+
+import java.io.IOException;
+
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.dingdong.common.dto.BaseResponseDto;
+import com.sparta.dingdong.common.exception.CommonErrorCode;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+
+// JwtAuthenticationEntryPoint가 없으면 인증실패시 시큐리티가 소셜로그인으로 리다이렉트
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+	private final ObjectMapper objectMapper;
+
+	@Override
+	public void commence(HttpServletRequest request,
+		HttpServletResponse response,
+		AuthenticationException authException) throws IOException {
+		response.setContentType("application/json;charset=UTF-8");
+		response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+		BaseResponseDto<?> error = BaseResponseDto.error(CommonErrorCode.UNAUTHORIZED);
+
+		response.getWriter().write(objectMapper.writeValueAsString(error));
+	}
+}
---
+++ src/main/java/com/sparta/dingdong/common/jwt/JwtUtil.java
@@ -0,0 +1,98 @@
+package com.sparta.dingdong.common.jwt;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import com.sparta.dingdong.domain.user.entity.enums.UserRole;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class JwtUtil {
+
+	@Value("${jwt.secret}")
+	private String secretKey;
+
+	private static final long EXPIRATION = 1000L * 60 * 30; // 30분
+	private static final long REFRESH_EXPIRATION = 1000L * 60 * 60 * 24 * 14; // 14일
+
+	public String createToken(Long id, UserRole userRole) {
+		return Jwts.builder()
+			.setSubject(String.valueOf(id))
+			.claim("userRole", userRole.name())
+			.setIssuedAt(new Date())
+
+			.setExpiration(new Date(System.currentTimeMillis() + EXPIRATION))
+
+			.signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+			.compact();
+	}
+
+	public UserAuth extractUserAuth(String token) {
+		Claims claims = Jwts.parserBuilder()
+			.setSigningKey(secretKey.getBytes())
+			.build()
+			.parseClaimsJws(token)
+			.getBody();
+
+		return new UserAuth(Long.parseLong(claims.getSubject()),
+			UserRole.valueOf(claims.get("userRole", String.class)));
+	}
+
+	public boolean validateToken(String token) {
+		try {
+			extractUserAuth(token);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	public String extractToken(HttpServletRequest request) {
+		String bearer = request.getHeader("Authorization");
+		if (bearer != null && bearer.startsWith("Bearer ")) {
+			return bearer.substring(7);
+		}
+		return null;
+	}
+
+	public long getExpiration(String token) {
+		Claims claims = Jwts.parserBuilder()
+			.setSigningKey(secretKey.getBytes())
+			.build()
+			.parseClaimsJws(token)
+			.getBody();
+
+		return claims.getExpiration().getTime() - System.currentTimeMillis();
+	}
+
+	public String createRefreshToken(Long id, UserRole role) {
+		return Jwts.builder()
+			.setSubject(String.valueOf(id))
+			.claim("userRole", role.name())
+			.claim("jti", UUID.randomUUID().toString())
+			.setIssuedAt(new Date())
+			.setExpiration(new Date(System.currentTimeMillis() + REFRESH_EXPIRATION))
+			.signWith(Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8)))
+			.compact();
+	}
+
+	public long getRefreshExpiration(String refreshToken) {
+		Claims claims = Jwts.parserBuilder()
+			.setSigningKey(secretKey.getBytes(StandardCharsets.UTF_8))
+			.build()
+			.parseClaimsJws(refreshToken)
+			.getBody();
+
+		return claims.getExpiration().getTime() - System.currentTimeMillis();
+	}
+}
