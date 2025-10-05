c142f0b929d2846b6a969358cf88b7acc67cef00
malaheaven <yunhg95@gmail.com>
2025-10-05 15:24:28 +0900

    feat: @EnableMethodSecurity 추가

--- src/main/java/com/sparta/dingdong/common/security/SecurityConfig.java
+++ src/main/java/com/sparta/dingdong/common/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.sparta.dingdong.common.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -20,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity
 public class SecurityConfig {
 
 	private final JwtFilter jwtFilter;
