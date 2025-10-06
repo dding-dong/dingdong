ae9adf34a77c83bc19a2e2ac52c41b38822b5a1d
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2025-10-07 01:55:52 +0900

    add: 매니저 관리 기능 업데이트

--- src/main/java/com/sparta/dingdong/domain/user/dto/response/ManagerResponseDto.java
+++ src/main/java/com/sparta/dingdong/domain/user/dto/response/ManagerResponseDto.java
@@ -1,5 +1,7 @@
 package com.sparta.dingdong.domain.user.dto.response;
 
+import java.time.LocalDateTime;
+
 import com.sparta.dingdong.domain.user.entity.Manager;
 import com.sparta.dingdong.domain.user.entity.enums.ManagerStatus;
 
@@ -10,10 +12,12 @@ public class ManagerResponseDto {
 
 	private Long id;
 	private ManagerStatus status;
+	private LocalDateTime createdAt;
 
 	public ManagerResponseDto(Manager entity) {
 		this.id = entity.getId();
 		this.status = entity.getManagerStatus();
+		this.createdAt = entity.getCreatedAt();
 	}
 }
 
