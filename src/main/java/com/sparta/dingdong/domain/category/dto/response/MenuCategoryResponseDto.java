b55612d622d52696e33cdb67a728bdb7c3f380c0
chachohee <cxoxee@gmail.com>
2025-10-07 12:12:00 +0900

    reafactor: category dto 어노테이션 통일

--- src/main/java/com/sparta/dingdong/domain/category/dto/response/MenuCategoryResponseDto.java
+++ src/main/java/com/sparta/dingdong/domain/category/dto/response/MenuCategoryResponseDto.java
@@ -5,12 +5,10 @@ import java.util.UUID;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Getter;
+import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
-@Getter
-@Setter
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
