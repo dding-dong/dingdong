4378e1bff6c716e28d33ee4ad8c17310e5bf1eff
believeme <cain1227@naver.com>
2025-10-09 10:04:56 +0900

    feat: #49 회원가입 중복이메일검사 추가, Dto 네이밍 수정

--- src/main/java/com/sparta/dingdong/domain/user/service/UserService.java
+++ src/main/java/com/sparta/dingdong/domain/user/service/UserService.java
@@ -4,12 +4,21 @@ import org.springframework.stereotype.Service;
 
 import com.sparta.dingdong.common.jwt.UserAuth;
 import com.sparta.dingdong.domain.user.dto.request.UserCreateRequestDto;
+import com.sparta.dingdong.domain.user.dto.request.UserUpdateRequestDto;
 import com.sparta.dingdong.domain.user.dto.response.UserResponseDto;
 import com.sparta.dingdong.domain.user.entity.User;
 
 @Service
 public interface UserService {
-	UserResponseDto createUser(UserCreateRequestDto requestDto);
+	void createUser(UserCreateRequestDto requestDto);
+
+	void updateUser(UserUpdateRequestDto request, UserAuth userAuth);
+
+	void checkPassword(String rawPassword, String hashedPassword);
+
+	void deleteUser(UserAuth userAuth);
+
+	UserResponseDto findById(UserAuth userAuth);
 
 	User findByUser(UserAuth userAuth);
 }
