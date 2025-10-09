f1063248e1c741f4594facdcc744fd11845bc6a4
malaheaven <yunhg95@gmail.com>
2025-10-09 17:13:32 +0900

    refactor: order, payment entity 수정

--- src/main/java/com/sparta/dingdong/domain/payment/entity/Payment.java
+++ src/main/java/com/sparta/dingdong/domain/payment/entity/Payment.java
@@ -1,18 +1,25 @@
 package com.sparta.dingdong.domain.payment.entity;
 
 import java.math.BigInteger;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 import com.sparta.dingdong.domain.order.entity.Order;
+import com.sparta.dingdong.domain.payment.entity.enums.FailReason;
 import com.sparta.dingdong.domain.payment.entity.enums.PaymentStatus;
+import com.sparta.dingdong.domain.payment.entity.enums.PaymentType;
+import com.sparta.dingdong.domain.user.entity.User;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -24,16 +31,29 @@ public class Payment {
 	@GeneratedValue(strategy = GenerationType.UUID)
 	private UUID id;
 
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
 	@OneToOne
 	@JoinColumn(name = "order_id")
 	private Order order;
 
-	private String payMethod;
+	@Enumerated(EnumType.STRING)
+	private PaymentType paymentType;
 
-	private BigInteger price;
-
-	private String pgTxId;
+	private BigInteger amount;
 
 	@Enumerated(EnumType.STRING)
 	private PaymentStatus paymentStatus;
+
+	@Column(name = "payment_key")
+	private String paymentKey;
+
+	@Enumerated(EnumType.STRING)
+	private FailReason failReason;
+
+	private String refundReason;
+
+	private LocalDateTime approvedAt;
 }
