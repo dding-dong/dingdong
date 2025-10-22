d579610c2e227557606ff7257840015bdf722db8
gahyeon <1000ji1@naver.com>
2025-10-22 13:39:38 +0900

    test: #87 @SpringBootTest -> 단위테스트로 전환

--- src/test/java/com/sparta/dingdong/domain/cart/service/CartServiceImplTest.java
+++ src/test/java/com/sparta/dingdong/domain/cart/service/CartServiceImplTest.java
@@ -9,12 +9,12 @@ import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import com.sparta.dingdong.common.TestDataFactory;
-import com.sparta.dingdong.common.TestJasyptConfig;
 import com.sparta.dingdong.common.jwt.UserAuth;
 import com.sparta.dingdong.domain.cart.dto.request.AddCartItemRequestDto;
 import com.sparta.dingdong.domain.cart.dto.response.CartItemResponseDto;
@@ -31,18 +31,19 @@ import com.sparta.dingdong.domain.store.entity.Store;
 import com.sparta.dingdong.domain.user.entity.User;
 import com.sparta.dingdong.domain.user.service.UserService;
 
-@SpringBootTest(classes = {CartServiceImpl.class, TestJasyptConfig.class})
+@ExtendWith(MockitoExtension.class)
+@DisplayName("CartService 테스트")
 class CartServiceImplTest {
 
-	@Autowired
-	CartService cartService;
+	@InjectMocks
+	CartServiceImpl cartService;
 
-	@MockBean
+	@Mock
 	CartRepository cartRepository;
 
-	@MockBean
+	@Mock
 	MenuItemService menuItemService;
-	@MockBean
+	@Mock
 	UserService userService;
 
 	private User customer;
@@ -73,42 +74,7 @@ class CartServiceImplTest {
 		userAuth = TestDataFactory.createUserAuth(customer);
 	}
 
-	@DisplayName("장바구니 있으면 조회")
-	@Test
-	void getCart_success() {
-		//given
-		when(userService.findByUser(any(UserAuth.class))).thenReturn(customer);
-		when(menuItemService.findById(kimchi.getId())).thenReturn(kimchi);
-
-		AddCartItemRequestDto request = AddCartItemRequestDto.builder()
-			.menuItemId(kimchi.getId())
-			.quantity(2)
-			.build();
-
-		when(cartRepository.save(any(Cart.class)))
-			.thenAnswer(invocation -> invocation.getArgument(0));
-
-		//when
-		CartResponseDto response = cartService.addItem(userAuth, request, false);
-
-		//then
-		assertNotNull(response);
-		assertEquals(store.getId(), response.getStoreId());
-		assertEquals(store.getName(), response.getStoreName());
-		assertEquals(1, response.getItems().size());
-
-		CartItemResponseDto itemResponse = response.getItems().get(0);
-		assertEquals(kimchi.getId(), itemResponse.getMenuItemId());
-		assertEquals(2, itemResponse.getQuantity());
-
-		BigInteger expectedTotal = kimchi.getPrice().multiply(BigInteger.valueOf(2));
-		assertEquals(expectedTotal, response.getTotalPrice());
-
-		verify(cartRepository, times(1)).findByUserId(customer.getId());
-		verify(userService, times(1)).findByUser(any(UserAuth.class));
-	}
-
-	@DisplayName("장바구니 없으면 예외처리")
+	@DisplayName("장바구니에 메뉴 담아둔거 없으면 조회되지 않아 예외처리")
 	@Test
 	void getCart_exception() {
 		// given
@@ -121,9 +87,48 @@ class CartServiceImplTest {
 		verify(cartRepository, times(1)).findByUserId(customer.getId());
 	}
 
+	@DisplayName("장바구니가 없을 때 addItem을 호출하면 새 장바구니가 생성된다.")
+	@Test
+	void addItem_whenCartNotExists_shouldCreateNewCart() {
+		final int QTY_2 = 2;
+
+		//given
+		when(userService.findByUser(any(UserAuth.class))).thenReturn(customer);
+		when(menuItemService.findById(kimchi.getId())).thenReturn(kimchi);
+
+		AddCartItemRequestDto request = AddCartItemRequestDto.builder()
+			.menuItemId(kimchi.getId())
+			.quantity(2)
+			.build();
+
+		when(cartRepository.save(any(Cart.class))).thenAnswer(invocation -> invocation.getArgument(0));
+
+		//when
+		CartResponseDto response = cartService.addItem(userAuth, request, false);
+
+		//then
+		assertNotNull(response);
+		assertEquals(store.getId(), response.getStoreId());
+		assertEquals(store.getName(), response.getStoreName());
+		assertEquals(1, response.getItems().size());
+
+		CartItemResponseDto itemResponse = response.getItems().get(0);
+		assertEquals(kimchi.getId(), itemResponse.getMenuItemId());
+		assertEquals(QTY_2, itemResponse.getQuantity());
+
+		BigInteger expectedTotal = kimchi.getPrice().multiply(BigInteger.valueOf(QTY_2));
+		assertEquals(expectedTotal, response.getTotalPrice());
+
+		verify(cartRepository, times(1)).findByUserId(customer.getId());
+		verify(userService, times(1)).findByUser(any(UserAuth.class));
+		verify(menuItemService, times(1)).findById(kimchi.getId());
+	}
+
 	@DisplayName("장바구니에 다른 가게가 있으면 replace=false일 때 예외처리")
 	@Test
 	void addItem_replace_false() {
+		final int QTY_2 = 2;
+
 		// given
 		when(userService.findByUser(any(UserAuth.class))).thenReturn(customer);
 		when(menuItemService.findById(kimchi.getId())).thenReturn(kimchi);
@@ -134,9 +139,8 @@ class CartServiceImplTest {
 
 		AddCartItemRequestDto request = AddCartItemRequestDto.builder()
 			.menuItemId(kimchi.getId())
-			.quantity(2)
+			.quantity(QTY_2)
 			.build();
-		when(cartRepository.save(any(Cart.class))).thenAnswer(inv -> inv.getArgument(0));
 
 		// when & then
 		assertThrows(CartStoreConflictException.class, () -> cartService.addItem(userAuth, request, false));
@@ -151,6 +155,8 @@ class CartServiceImplTest {
 	@DisplayName("장바구니에 다른 가게가 있으면 replace=true일 때 기존 장바구니 교체하고 추가")
 	@Test
 	void addItem_replace_true() {
+		final int QTY_2 = 2;
+
 		// given
 		when(userService.findByUser(any(UserAuth.class))).thenReturn(customer);
 		when(menuItemService.findById(kimchi.getId())).thenReturn(kimchi);
@@ -159,13 +165,12 @@ class CartServiceImplTest {
 		Cart existingCart = TestDataFactory.createCart(customer, store2, jjajang);
 
 		when(cartRepository.findByUserId(customer.getId())).thenReturn(Optional.of(existingCart));
-
 		when(cartRepository.save(any(Cart.class))).thenAnswer(inv -> inv.getArgument(0));
 
 		// store1 메뉴 장바구니에 추가
 		AddCartItemRequestDto request = AddCartItemRequestDto.builder()
 			.menuItemId(kimchi.getId())
-			.quantity(2)
+			.quantity(QTY_2)
 			.build();
 
 		// when
@@ -179,9 +184,9 @@ class CartServiceImplTest {
 
 		CartItemResponseDto itemResponse = response.getItems().get(0);
 		assertEquals(kimchi.getId(), itemResponse.getMenuItemId());
-		assertEquals(2, itemResponse.getQuantity());
+		assertEquals(QTY_2, itemResponse.getQuantity());
 
-		BigInteger expectedTotal = kimchi.getPrice().multiply(BigInteger.valueOf(2));
+		BigInteger expectedTotal = kimchi.getPrice().multiply(BigInteger.valueOf(QTY_2));
 		assertEquals(expectedTotal, response.getTotalPrice());
 
 		verify(cartRepository, times(1)).findByUserId(customer.getId());
@@ -191,23 +196,19 @@ class CartServiceImplTest {
 	@DisplayName("장바구니 메뉴 수량 변경 성공")
 	@Test
 	void updateItemQuantity_success() {
+		final int QTY_3 = 3;
+
 		//given
 		when(userService.findByUser(any(UserAuth.class))).thenReturn(customer);
-		when(menuItemService.findById(kimchi.getId())).thenReturn(kimchi);
 
 		Cart existingCart = TestDataFactory.createCart(customer, store, kimchi);
 		existingCart.addItem(kimchi, 2);
 
-		when(cartRepository.findByUserId(customer.getId()))
-			.thenReturn(Optional.of(existingCart));
-
-		when(cartRepository.save(any(Cart.class)))
-			.thenAnswer(invocation -> invocation.getArgument(0));
-
-		int newQuantity = 3;
+		when(cartRepository.findByUserId(customer.getId())).thenReturn(Optional.of(existingCart));
+		when(cartRepository.save(any(Cart.class))).thenAnswer(invocation -> invocation.getArgument(0));
 
 		// when
-		CartResponseDto response = cartService.updateItemQuantity(userAuth, kimchi.getId(), newQuantity);
+		CartResponseDto response = cartService.updateItemQuantity(userAuth, kimchi.getId(), QTY_3);
 
 		// then
 		assertNotNull(response);
@@ -216,37 +217,32 @@ class CartServiceImplTest {
 
 		CartItemResponseDto updatedItem = response.getItems().get(0);
 		assertEquals(kimchi.getId(), updatedItem.getMenuItemId());
-		assertEquals(newQuantity, updatedItem.getQuantity());
+		assertEquals(QTY_3, updatedItem.getQuantity());
 
-		BigInteger expectedTotal = kimchi.getPrice().multiply(BigInteger.valueOf(newQuantity));
+		BigInteger expectedTotal = kimchi.getPrice().multiply(BigInteger.valueOf(QTY_3));
 		assertEquals(expectedTotal, response.getTotalPrice());
 
 		verify(cartRepository, times(1)).findByUserId(customer.getId());
 		verify(cartRepository, times(1)).save(any(Cart.class));
 	}
 
-	@DisplayName("장바구니 메뉴 수량 0으로 변경 예외처리")
+	@DisplayName("장바구니 메뉴 수량 0으로 변경하면 예외처리")
 	@Test
 	void updateItemQuantity_exception() {
+		final int QTY_0 = 0;
+
 		//given
 		when(userService.findByUser(any(UserAuth.class))).thenReturn(customer);
-		when(menuItemService.findById(kimchi.getId())).thenReturn(kimchi);
 
 		Cart existingCart = TestDataFactory.createCart(customer, store, kimchi);
 		existingCart.addItem(kimchi, 2);
 
-		when(cartRepository.findByUserId(customer.getId()))
-			.thenReturn(Optional.of(existingCart));
-
-		when(cartRepository.save(any(Cart.class)))
-			.thenAnswer(invocation -> invocation.getArgument(0));
-
-		int newQuantity = 0;
+		when(cartRepository.findByUserId(customer.getId())).thenReturn(Optional.of(existingCart));
 
 		// when & then
 		assertThrows(
 			InvalidCartQuantityException.class,
-			() -> cartService.updateItemQuantity(userAuth, kimchi.getId(), newQuantity)
+			() -> cartService.updateItemQuantity(userAuth, kimchi.getId(), QTY_0)
 		);
 
 		verify(cartRepository, times(1)).findByUserId(customer.getId());
